b9934c14339709faf5eb8d9c5a868d5fe9e8d580
ArtemKoshevoi <artem.kos@2muchcoffee.com>
2021-02-03 14:22:16 +0200

    add redux and recorder component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+import Recorder from '../Recorder';
 
 function App() {
-  return <div className="App">learn react</div>;
+  return (
+    <div className="App">
+      <Recorder />
+    </div>
+  );
 }
 
 export default App;
